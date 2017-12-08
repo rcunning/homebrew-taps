e4d6fe4df7400ac3d190e3d4f8e050837ff44e33
Ryan Cunningham <rcunning@gmail.com>
2017-12-08 13:37:35 -0800

    update to get working with ffmpeg head

--- vmaf.rb
+++ vmaf.rb
@@ -1,9 +1,9 @@
 class Vmaf < Formula
   desc "Perceptual video quality assessment based on multi-method fusion."
   homepage "https://github.com/Netflix/vmaf#usage-through-libvmaf"
-  url "https://github.com/rcunning/homebrew-taps/raw/master/archive/vmaf-1.0.0.tar.xz"
-  sha256 "224e6d040d3418dab48709309b7a74031e8733e4f3c0c2d7776513c6a8877beb"
-  version "1.0.0"
+  url "https://github.com/rcunning/homebrew-archives/raw/master/vmaf-1.0.1.tar.xz"
+  sha256 "cf66dbd0d4947f433ba97b45e138f0ae7a2fc1a163ca2382c213c33b2a64960e"
+  version "1.0.1"
   head "https://github.com/Netflix/vmaf.git"
 
   depends_on "pkg-config" => :build
